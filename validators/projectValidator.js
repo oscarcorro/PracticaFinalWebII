044441f8751eec8f29945b1dd8fa820d974a0224
oscarcorro <oscarcorrochanolopez@gmail.com>
2025-04-14 07:40:11 +0200

    Validador de proyectos

---
+++ validators/projectValidator.js
@@ -0,0 +1,16 @@
+const {check} = require("express-validator")
+
+//validador antes de crear un proyecto
+const validatorCreateProyect = [
+    check("name").notEmpty().withMessage("El nombre del proyecto es obligatorio"),
+    check("client").notEmpty().withMessage("El cliente es obligatorio"),
+    check("description").optional().isString().withMessage("La descripción debe ser un texto")
+]
+//validador para actualizar un proyecto
+const validatorUpdateProject = [
+    check("name").notEmpty().withMessage("El nombre del proyecto es obligatorio"),
+    check("client").notEmpty().withMessage("El cliente es obligatorio"),
+    check("description").optional().isString().withMessage("La descripción debe ser un texto")
+]
+
+module.exports = {validatorCreateProyect, validatorUpdateProject}
